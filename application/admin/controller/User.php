4c93891d6793ee2e34b8228574f71ad316e52dc4
KaiChow <zhoukai37927@163.com>
2023-07-01 13:53:57 +0800

    获取配置

---
+++ application/admin/controller/User.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\admin\controller;
+
+use think\facade\Config;
+
+class User
+{
+    public function get(){
+        // 获取所有的配置
+        // dump(Config::get());
+        // 获取app的胚子
+        // dump(Config::get('app.'));
+        // 仅仅获取一级配置
+        // dump(Config::pull('app'));
+        dump(Config::get('database.hostname'));
+    }
+    
+    public function set(){
+        // 动态配置
+        // dump(Config::set('app_debug',false));
+    }
+
+    public function helper(){
+        // 助手函数不依赖Config
+        dump(config());
+    }
+}
